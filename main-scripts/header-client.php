d3d63afbadca3ccb2e821ee2e5c5118bf578f12e
gxescdn <117129712+gxescdn@users.noreply.github.com>
2022-11-11 22:05:06 +0100

    Update header-client.php

--- main-scripts/header-client.php
+++ main-scripts/header-client.php
@@ -1,5 +1,3 @@
 document.write(`
-<link href="style.css" rel="stylesheet" type="text/css" />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://api.escdn.xyz/domain-client/none-client.js"></script>
+<script src="https://gxcdn.xyz/main-header/header-style"></script>
 `)
