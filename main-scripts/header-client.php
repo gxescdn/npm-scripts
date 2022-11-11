c7afc1a051ce91790306006bef26ab89cac3e669
gxescdn <117129712+gxescdn@users.noreply.github.com>
2022-11-11 21:39:22 +0100

    Create header-client.php

---
+++ main-scripts/header-client.php
@@ -0,0 +1,5 @@
+document.write(`
+<link href="style.css" rel="stylesheet" type="text/css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://api.escdn.xyz/domain-client/none-client.js"></script>
+`)
